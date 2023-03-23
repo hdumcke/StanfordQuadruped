4e64bb7a708b418c16d42d8d2f43da2bcc07ab49
Horst Dumcke <hdumcke@marlysys.com>
2023-03-23 11:44:10 +0100

    testing

--- install.sh
+++ install.sh
@@ -32,20 +32,7 @@ git clone https://github.com/Tiryoh/ros2_setup_scripts_ubuntu.git
 ./ros2_setup_scripts_ubuntu/run.sh
 
 source /opt/ros/humble/setup.bash
-mkdir -p ~/mini_pupper_ws/src
 cd ~/mini_pupper_ws
-cp -r $BASEDIR/ros2/mini_pupper src/
-mkdir -p src/mini_pupper/mini_pupper/src
-mkdir -p src/mini_pupper/mini_pupper/pupper
-cp -r $BASEDIR/src/IMU.py src/mini_pupper/mini_pupper/src
-cp -r $BASEDIR/src/Controller.py src/mini_pupper/mini_pupper/src
-cp -r $BASEDIR/src/State.py src/mini_pupper/mini_pupper/src
-cp -r $BASEDIR/src/Gaits.py src/mini_pupper/mini_pupper/src
-cp -r $BASEDIR/src/StanceController.py src/mini_pupper/mini_pupper/src
-cp -r $BASEDIR/src/SwingLegController.py src/mini_pupper/mini_pupper/src
-cp -r $BASEDIR/src/Utilities.py src/mini_pupper/mini_pupper/src
-mkdir -p src/mini_pupper/mini_pupper/pupper
-cp -r $BASEDIR/pupper/Kinematics.py src/mini_pupper/mini_pupper/pupper
 rosdep update && rosdep install --from-path src --ignore-src -y --skip-keys microxrcedds_agent --skip-keys micro_ros_agent
 sudo pip install setuptools==58.2.0 # suppress colcon build warning
 colcon build --executor sequential --symlink-install
