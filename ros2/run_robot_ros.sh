07a2fc27ec0b718aa5cd99b23096ef601cd08372
Horst Dumcke <hdumcke@marlysys.com>
2023-03-31 15:49:38 +0200

    testing

--- ros2/run_robot_ros.sh
+++ ros2/run_robot_ros.sh
@@ -3,4 +3,4 @@
 source ~/StanfordQuadruped/mini_pupper_ws/install/setup.bash
 rm -rf ~/.ros/log/*
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-ros2 launch mini_pupper_bringup bringup.launch.py
+ros2 launch mini_pupper_bringup bringup.launch.py dt:=0.01
