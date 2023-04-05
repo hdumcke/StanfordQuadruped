b71201bdd87cfcc09ade7b1f25d75066d0fdc4ec
Horst Dumcke <hdumcke@marlysys.com>
2023-04-05 18:05:17 +0200

    working on testing

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,37 +1,3 @@
 #!/bin/bash
 
-# check Ubuntu version
-source /etc/os-release
-
-if [[ $UBUNTU_CODENAME != 'jammy' ]]
-then
-    echo "Ubuntu 22.04.1 LTS (Jammy Jellyfish) is required"
-    echo "You are using $VERSION"
-    exit 1
-fi
-
-sudo apt-get update
-sudo apt-get -y install bridge-utils python3 python-is-python3 python3-venv python3-virtualenv
-sudo brctl addbr br0
-sudo ip addr add 10.0.0.10/24 dev br0
-sudo ip link add dummy1 type dummy
-sudo ip link set dev dummy1 master br0
-sudo ip link set dummy1 up
-sudo ip link set br0 up
-
-cd ~
-[[ -d ~/StanfordQuadruped ]] || git clone https://github.com/mangdangroboticsclub/StanfordQuadruped.git
-[[ -d ~/mini_pupper_venv ]] || python -m venv ~/mini_pupper_venv
-[[ -d ~/UDPComms ]] || git clone https://github.com/stanfordroboticsclub/UDPComms.git
-[[ -d ~/mini_pupper_web_controller ]] || git clone https://github.com/mangdangroboticsclub/mini_pupper_web_controller.git
-
-source  ~/mini_pupper_venv/bin/activate
-pip install numpy transforms3d pigpio pyserial coverage pytest netifaces msgpack
-pip install ~/UDPComms/
-pip install ~/mini_pupper_web_controller/joystick_sim
-echo "import coverage; coverage.process_startup()" > ~/mini_pupper_venv/lib/python3.10/site-packages/.pth
-cd ~/StanfordQuadruped/tests/integration_tests
-COVERAGE_PROCESS_START=$(pwd)/.coveragerc coverage run -m pytest -v
-coverage combine
-coverage report
-coverage html
+ROS_DOMAIN_ID=0 ros2 topic pub --rate 50 -t 100 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.2, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
