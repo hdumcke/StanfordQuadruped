14de51f38dcc66dd7df3eaa0574f157da30d6454
Horst Dumcke <hdumcke@marlysys.com>
2023-04-06 10:06:30 +0200

    working on testing

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+sudo systemctl stop robot-ros
+rm /tmp/HardwareInterface.log
+rm /tmp/Display.log
+sudo systemctl start robot-ros
+sleep 10
 ROS_DOMAIN_ID=0 ros2 topic pub --rate 50 -t 100 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.2, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
+sleep 20
+sudo systemctl stop robot-ros
