6081da145ac067dcd114bcf8748e54f3d9e354e0
Horst Dumcke <hdumcke@marlysys.com>
2023-04-10 14:33:39 +0200

    WIP

---
+++ setup_pc.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+######################################################################################
+# Stanford
+#
+# This stack will consist of board support package (mini_pupper_bsp),
+#    the StanfordQuadruped controller and the mini_pupper_web_controller
+#
+# After installation you can either pair a supported PS4 joystick or
+# point your web browser to 
+#   http://x.x.x.x:8080
+# where x.x.x.x is the IP address of your Mini Pupper as displayed on the LCD screen
+#
+# To install
+#    ./setup.sh <SSID> "<your Wifi password>"
+######################################################################################
+
+set -e
+echo "setup.sh started at $(date)"
+
+### Get directory where this script is installed
+BASEDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+### Install ROS2
+cd ~
+git clone https://github.com/Tiryoh/ros2_setup_scripts_ubuntu.git
+./ros2_setup_scripts_ubuntu/run.sh
+
+source /opt/ros/humble/setup.bash
+cd ~/StanfordQuadruped/mini_pupper_ws
+rosdep update && rosdep install --from-path src --ignore-src -y --skip-keys microxrcedds_agent --skip-keys micro_ros_agent
+sudo pip install setuptools==58.2.0 # suppress colcon build warning
+colcon build --symlink-install
