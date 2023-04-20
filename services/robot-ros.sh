15ab876ce97e92b032541e6c98c3afc5aa96e078
Horst Dumcke <hdumcke@marlysys.com>
2023-04-20 21:08:14 +0200

    testing

--- services/robot-ros.sh
+++ services/robot-ros.sh
@@ -3,5 +3,5 @@
 source ~/StanfordQuadruped/mini_pupper_ws/install/setup.bash
 rm -rf ~/.ros/log/*
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export ROS_DOMAIN_ID=0
+export ROS_DOMAIN_ID=$(cat /home/ubuntu/mini_pupper_ros_bsp/services/ROS_DOMAIN_ID)
 ros2 launch mini_pupper_bringup bringup.launch.py dt:=0.01
