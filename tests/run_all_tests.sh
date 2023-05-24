867ff33c128a6056e7be017faf02cdd70666464d
Horst Dumcke <hdumcke@marlysys.com>
2023-05-24 08:19:57 +0200

    WIP

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 
-source /opt/ros/humble/setup.bash
-export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-export ROS_DOMAIN_ID=0
+source  ~/mini-pupper-release
+if [ "$MACHINE" == "x86_64" ]
+then
+ACTIVE="rc-local lidar v4l2_camera robot-ros mp-joy"
+ACTIVATING="battery_monitor"
+if [ "$HARDWARE" == "mini_pupper_2" ]
+then
+ACTIVE=$ACTIVE" imu"
+ACTIVATING=$ACTIVATING" esp32-proxy"
+fi
+else
+ACTIVE="rc-local lidar v4l2_camera robot-ros mp-joy"
+ACTIVATING="battery_monitor"
+if [ "$HARDWARE" == "mini_pupper_2" ]
+then
+ACTIVE=$ACTIVE" imu"
+ACTIVATING=$ACTIVATING" esp32-proxy"
+fi
+fi
 
-sudo systemctl stop robot-ros
-rm /tmp/HardwareInterface.log
-rm /tmp/Display.log
-sudo systemctl start robot-ros
+# Give services time to start
 sleep 10
-ros2 topic pub --rate 50 -t 100 /cmd_vel geometry_msgs/msg/Twist "{linear: {x: 0.2, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.0}}"
-sleep 20
-sudo systemctl stop robot-ros
+
+for SERVICE in $ACTIVE; do
+    STATE=$(sudo systemctl show $SERVICE | grep ActiveState | awk -F'=' '{print $2}')
+    if [ "$STATE" == "active" ]
+    then
+	echo $SERVICE is OK
+    else
+	echo $SERVICE is NOT OK
+    fi
+done
+
+for SERVICE in $ACTIVATING; do
+    STATE=$(sudo systemctl show $SERVICE | grep ActiveState | awk -F'=' '{print $2}')
+    if [ "$STATE" == "activating" ]
+    then
+	echo $SERVICE is OK
+    else
+	echo $SERVICE is NOT OK
+    fi
+done
