887bdf23b6eac55f625e16c9b2daa79389eb5a83
Horst Dumcke <hdumcke@marlysys.com>
2023-05-24 13:57:07 +0200

    Revert "working on vpn issues"
    This reverts commit dddda6ee5d7d4caae45ee73b564963bcf975f361.

--- tests/vpn/config_cyclondds.sh
+++ tests/vpn/config_cyclondds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/StanfordQuadruped/services
-sed -i "/RMW_IMPLEMENTATION/a export CYCLONEDDS_URI=file:///home/ubuntu/StanfordQuadruped/tests/vpn/cyclone_dds_config_server.xml" *.sh
+sed -i "/RMW_IMPLEMENTATION/a export CYCLONEDDS_URI=file:///home/ubuntu/StanfordQuadruped/tests/vpn/cyclone_dds_config.xml" *.sh
 cd ~/mini_pupper_ros_bsp/services
-sed -i "/RMW_IMPLEMENTATION/a export CYCLONEDDS_URI=file:///home/ubuntu/StanfordQuadruped/tests/vpn/cyclone_dds_config_server.xml" *.sh
-echo "export CYCLONEDDS_URI=file:///home/ubuntu/StanfordQuadruped/tests/vpn/cyclone_dds_config_server.xml" >> ~/.bashrc
+sed -i "/RMW_IMPLEMENTATION/a export CYCLONEDDS_URI=file:///home/ubuntu/StanfordQuadruped/tests/vpn/cyclone_dds_config.xml" *.sh
+echo "export CYCLONEDDS_URI=file:///home/ubuntu/StanfordQuadruped/tests/vpn/cyclone_dds_config.xml" >> ~/.bashrc
